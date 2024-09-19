56367fbd6638d7c50a9039ead7ab0ff09e2111f4
OtavioAL <otavioauglei@gmail.com>
2024-09-19 19:41:39 -0300

    feat: constants with data consumed in the project (restaurants and food)

---
+++ food/src/constants/restaurants.ts
@@ -0,0 +1,32 @@
+export const RESTAURANTS = [
+  {
+    id: "1",
+    name: "KFC",
+    image: "https://i.imgur.com/sZ1Jye3.png",
+  },
+  {
+    id: "2",
+    name: "McDonald's",
+    image: "https://i.imgur.com/su4iB3p.png",
+  },
+  {
+    id: "3",
+    name: "Outback",
+    image: "https://i.imgur.com/jg3GJVN.png",
+  },
+  {
+    id: "4",
+    name: "Sushi Dev",
+    image: "https://i.imgur.com/ffYWFBX.png",
+  },
+  {
+    id: "5",
+    name: "Burguer Grill",
+    image: "https://i.imgur.com/kmQvG2T.png",
+  },
+  {
+    id: "6",
+    name: "Marmitas express",
+    image: "https://i.imgur.com/uXRuDFD.png",
+  },
+];
